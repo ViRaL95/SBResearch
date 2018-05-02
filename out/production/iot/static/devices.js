af541830d84529991ccc850dfd3d767dae02e18a
Varun <varun.rao@stonybrook.edu>
2018-05-02 09:31:19 -0400

    fixed untracked files

--- out/production/iot/static/devices.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    #("#device_info").hide();
-    $("#edit").click(function()[
-        id_number = $(this).siblings("#id");
-        make_request(id_number);
-        $("#device_info").show();
-    });
-});
